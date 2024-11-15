6a80deec8fd30b308db9f6c379b1a2d1f4305dec
tanakanaoki032 <mcd2476032@stu.o-hara.ac.jp>
2024-11-15 11:15:32 +0900

    Commit

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from django.views.generic import CreateView, TemplateView
+from .forms import CuntomUserCreationForm
+from django.urls import reverse_lazy
+# Create your views here.
+
+class SignUpView(CreateView):
+    form_class = CuntomUserCreationForm
+    template_name = "signup.html"
+    success_url = reverse_lazy('accounts:signup_success')
+    def form_valid(self, form):
+        user = form.save()
+        self.object = user
+        return super().form_valid(form)
+class SignUpSuccessView(TemplateView):
+    template_name = "signup_success.html"
